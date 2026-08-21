7f255fa0eb47636d950a8d78eb23f1f1b6d5e010
Adjanouhoun <amadouadjanouhoun@gmail.com>
2026-08-21 16:09:17 +0200

    feat: initialiser la refonte trilingue de Fyracare

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/{_locale}', name: 'app_home', requirements: ['_locale' => 'fr|en|ar'], defaults: ['_locale' => 'fr'])]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
